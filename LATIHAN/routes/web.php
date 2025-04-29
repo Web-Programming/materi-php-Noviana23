7a3eec07571fa466f43aba044f5ed0bf34faf354
Noviana23 <noviphi889@gmail.com>
2025-04-29 20:36:02 +0700

    Pembuatan Beranda dan Berita

--- LATIHAN/routes/web.php
+++ LATIHAN/routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Home', action: function() {
+    return view(
+        'beranda',
+        [
+            'name' => 'Nopi',
+            'email' => 'noviphi889@gmail.com',
+            'alamat' => 'Jl. Rumah'
+        ]
+    );
+});
+
+Route::get('/berita/{id}/{judul}', function($id,$judul){
+    return view('berita', ['id'=>$id, 'judul'=>$judul]);
+});
